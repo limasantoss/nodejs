a28c3f31c6fdbc769a8b54730dd15b77c9bac0b4
Igor Santos <igorlimasantos667@gmail.com>
2025-08-05 16:44:53 -0300

    .

---
+++ 02modulos/app.js
@@ -0,0 +1,9 @@
+//Importar arquivo de Calculos
+var calculos = require('./calculos');
+
+//Obter a função para multiplicar
+var multiplicar = calculos.multiplicar;
+
+
+//realizar um calculo de multiplicaçõa
+console.log(multiplicar(5, 6))
